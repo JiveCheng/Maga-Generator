d786ba36e6d153ca8ae27b20ce0ad368e8d8c5c5
Jive <zhesheng.zheng@gmail.com>
2018-01-18 17:44:07 +0800

    do something

--- bin/command.js
+++ bin/command.js
@@ -1,1 +1,9 @@
 #! /usr/bin/env node
+const path = require('path');
+const fs = require('fs');
+const exec = require('child_process').exec;
+const program = require('commander');
+const readline = require('readline');
+const deepExtend = require('deep-extend');
+
+console.log('yes');
