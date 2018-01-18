a850510d3941c354d58d988952851b0e87f1b238
Jive <zhesheng.zheng@gmail.com>
2018-01-19 00:04:01 +0800

    Modified some

--- bin/command.js
+++ bin/command.js
@@ -6,4 +6,37 @@ const program = require('commander');
 const readline = require('readline');
 const deepExtend = require('deep-extend');
 
-console.log('yes');
+
+console.log(path.resolve(), path.dirname(process.argv[1]));
+
+let MagaGenerator = function(options) {
+	this.rl = readline.createInterface({
+		input:process.stdin,
+		output:process.stdout
+	});
+	this.interfaceString = {
+		home: `
+Please choose a module template:
+ 1) [non-component] style-only module of Magaele
+ 2) [non-component] jquery-plugin module of Magaele
+ 3) [component] react functional component of Magaele
+ 4) [component] react class component of Magaele
+ `
+	};
+	this.path = {
+		module: path.resolve(),
+		runtime: path.dirname(process.argv[1])
+	};
+};
+
+// rl.question(interfaceString.home, (answer) => {
+	
+// 	rl.close();
+// });	
+
+// rl.on("close", function(){
+// 	// 结束程序
+	
+// 	process.exit(0);
+// });
+console.log('runstart');
